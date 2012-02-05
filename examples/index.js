b2c9ffda6b044d5c965e2d74c6b508b406b854fd
koyachi <rtk2106@gmail.com>
2012-02-05 13:06:38 +0900

    use connect.static

--- examples/index.js
+++ examples/index.js
@@ -3,7 +3,8 @@ var monkeyglitch = require('../');
 
 connect(
     connect.logger()
-  , monkeyglitch(__dirname + '/public')
+  , monkeyglitch()
+  , connect.static(__dirname + '/public', {maxAge: 1})
   , function(req, res) {
     res.setHeader('Content-Type', 'text/html');
     res.end('<img src="/001.jpg" /><img src="/002.gif" /><img src="/003.png" />');
